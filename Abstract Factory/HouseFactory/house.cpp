0d101ed8f07b984aa16ee88254fca9018c47c9db
Michael Mandl <mandlm@molez.net>
2016-03-19 19:33:28 +0100

    Added numberOfFloors, numberOfBasements and toString Method

--- Abstract Factory/HouseFactory/house.cpp
+++ Abstract Factory/HouseFactory/house.cpp
@@ -1,6 +1,34 @@
 #include "house.h"
 
+#include <iostream>
+#include <sstream>
+
 House::House()
 {
+	std::cout << "Created a house" << std::endl;
+}
 
+House::~House()
+{
+	std::cout << "Destroyed a house" << std::endl;
+}
+
+void House::setNumerOfFloors(size_t numberOfFloors)
+{
+	m_numberOfFloors = numberOfFloors;
+}
+
+void House::setNumberOfBasements(size_t numberOfBasements)
+{
+	m_numberOfBasements = numberOfBasements;
+}
+
+std::string House::toString() const
+{
+	std::stringstream result;
+
+	result << "Floors: " << m_numberOfFloors << std::endl;
+	result << "Basements: " << m_numberOfBasements << std::endl;
+
+	return result.str();
 }
--- Abstract Factory/HouseFactory/house.h
+++ Abstract Factory/HouseFactory/house.h
@@ -1,7 +1,19 @@
 #pragma once
 
+#include <string>
+
 class House
 {
+private:
+	size_t m_numberOfFloors = 0;
+	size_t m_numberOfBasements = 0;
+
 public:
-    House();
+	House();
+	~House();
+
+	void setNumerOfFloors(size_t numberOfFloors);
+	void setNumberOfBasements(size_t numberOfBasements);
+
+	std::string toString() const;
 };
