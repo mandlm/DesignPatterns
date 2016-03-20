3b414f52acf2b16336462cdb5bda5ec90ff048fc
Michael Mandl <mandlm@molez.net>
2016-03-20 18:03:23 +0100

    Basic client

--- Builder/SceneExporter/main.cpp
+++ Builder/SceneExporter/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
+#include "scene.h"
+#include "objfileconverter.h"
+#include "scenereader.h"
+
 int main(int argc, char **argv)
 {
-    std::cout << "Hello SceneExporter" << std::endl;
-	
+    Scene scene = Scene::createSampleScene();
+
+    ObjFileConverter objFileConverter;
+    SceneReader sceneToObjFileReader(scene, objFileConverter);
+
+    sceneToObjFileReader.construct();
+
+    std::string objFileData = objFileConverter.getResult();
+
+    std::cout << "Obj file data:" << std::endl << std::endl;
+
+    std::cout << objFileData << std::endl;
+
 	return 0;
 }
