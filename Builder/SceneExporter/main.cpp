5f0ebe93453fd4eb93fbc5457c8fa17e3ee246b0
Michael Mandl <mandlm@molez.net>
2016-03-20 18:14:18 +0100

    Collect and display scene statistics

--- Builder/SceneExporter/main.cpp
+++ Builder/SceneExporter/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 #include "scene.h"
+
 #include "objfileconverter.h"
+#include "statscollector.h"
+
 #include "scenereader.h"
 
 int main(int argc, char **argv)
@@ -10,14 +13,19 @@ int main(int argc, char **argv)
 
     ObjFileConverter objFileConverter;
     SceneReader sceneToObjFileReader(scene, objFileConverter);
-
     sceneToObjFileReader.construct();
-
     std::string objFileData = objFileConverter.getResult();
 
     std::cout << "Obj file data:" << std::endl << std::endl;
+    std::cout << objFileData << std::endl << std::endl;
 
-    std::cout << objFileData << std::endl;
+    StatsCollector statsCollector;
+    SceneReader statsCollectorReader(scene, statsCollector);
+    statsCollectorReader.construct();
+    std::string stats = statsCollector.getResult();
+
+    std::cout << "Stats:" << std::endl << std::endl;
+    std::cout << stats << std::endl << std::endl;
 
 	return 0;
 }
