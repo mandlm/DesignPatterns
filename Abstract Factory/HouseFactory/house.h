5520286b025a1f29777ccac89d823f4750f7c563
Michael Mandl <mandlm@molez.net>
2016-03-20 12:19:56 +0100

    Made house abstract

--- Abstract Factory/HouseFactory/house.h
+++ Abstract Factory/HouseFactory/house.h
@@ -4,16 +4,13 @@
 
 class House
 {
-private:
+protected:
 	size_t m_numberOfFloors = 0;
 	size_t m_numberOfBasements = 0;
 
 public:
-	House();
-	~House();
-
 	void setNumerOfFloors(size_t numberOfFloors);
 	void setNumberOfBasements(size_t numberOfBasements);
 
-	std::string toString() const;
+    virtual std::string toString() const = 0;
 };
