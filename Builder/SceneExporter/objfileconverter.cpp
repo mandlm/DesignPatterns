cf95119a34846e495e4b960c93975835071da195
Michael Mandl <mandlm@molez.net>
2016-03-20 18:27:05 +0100

    Export obj-file data

--- Builder/SceneExporter/objfileconverter.cpp
+++ Builder/SceneExporter/objfileconverter.cpp
@@ -2,17 +2,57 @@
 
 #include <sstream>
 
+ObjFileConverter::ObjFileConverter()
+{
+    m_vertexBuffer.reserve(32);
+}
+
 void ObjFileConverter::buildVertex(double x, double y, double z)
 {
+    m_vertexBuffer.push_back({x, y, z});
 }
 
 void ObjFileConverter::buildFace(size_t numberOfVertices)
 {
+    if (m_vertexBuffer.size() == numberOfVertices)
+    {
+        std::list<Vertex> vertices;
+
+        for (Vertex &vertex : m_vertexBuffer)
+        {
+            vertices.push_back(vertex);
+        }
+
+        m_faces.push_back(vertices);
+    }
+
+    m_vertexBuffer.clear();
 }
 
 std::string ObjFileConverter::getResult() const
 {
     std::stringstream result;
+
+    for (const std::list<Vertex> &face : m_faces)
+    {
+        for (const Vertex &vertex : face)
+        {
+            result << "v " << vertex.x << " " << vertex.y << " " << vertex.z << std::endl;
+        }
+    }
+
+    result << std::endl;
+
+    size_t offset = 1;
+    for (const std::list<Vertex> &face : m_faces)
+    {
+        result << "f";
+        for (size_t i = 0; i < face.size(); ++i)
+        {
+            result << " " << offset++;
+        }
+        result << std::endl;
+    }
 
     return result.str();
 }
--- Builder/SceneExporter/objfileconverter.h
+++ Builder/SceneExporter/objfileconverter.h
@@ -2,12 +2,26 @@
 
 #include <string>
 #include <vector>
+#include <list>
 
 #include "sceneconverter.h"
 
 class ObjFileConverter : public SceneConverter
 {
+private:
+    class Vertex
+    {
+    public:
+        double x, y, z;
+    };
+
+private:
+    std::vector<Vertex> m_vertexBuffer;
+    std::list<std::list<Vertex>> m_faces;
+
 public:
+    ObjFileConverter();
+
 	virtual void buildVertex(double x, double y, double z) override;	
 	virtual void buildFace(size_t numberOfVertices) override;
 
